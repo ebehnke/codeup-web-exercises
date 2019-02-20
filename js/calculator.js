fd01969497fd591c990a4fdabb709e9fbcfc0405
Erik Behnke <erik.behnke@gmail.com>
2019-02-20 12:37:00 -0600

    begin calculator js project

---
+++ js/calculator.js
@@ -0,0 +1,37 @@
+// IIFE / Remove when finished coding //
+// (function(){
+"use strict";
+
+/* Rules:
+    -no eval() function
+ */
+
+var numberButtons = document.getElementsByClassName('numBtn');
+var operatorButtons = document.getElementsByClassName('operatorBtn');
+var clearButton = document.getElementsByClassName('clearBtn');
+var num1 = document.getElementById('1');
+var num2 = document.getElementById('2');
+var num3 = document.getElementById('3');
+var num4 = document.getElementById('4');
+var num5 = document.getElementById('5');
+var num6 = document.getElementById('6');
+var num7 = document.getElementById('7');
+var num8 = document.getElementById('8');
+var num9 = document.getElementById('9');
+var num0 = document.getElementById('0');
+var plus = document.getElementById('+');
+var minus = document.getElementById('-');
+var multiply = document.getElementById('*');
+var divide = document.getElementById('/');
+var equals = document.getElementsById('=');
+
+var num1ButtonClick = function(e){
+    //type number into text box
+};
+
+num1.addEventListener('click', num1ButtonClick, false);
+
+
+
+// IIFE closing //
+// })();
