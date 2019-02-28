a3ad4c6fe150aeefb5ae40483dfaa3bd16a34b9e
Erik Behnke <erik.behnke@gmail.com>
2019-02-28 15:03:50 -0600

    begin jquery-faq.html

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,21 @@
+// IIFE / Remove when finished coding //
+// (function(){
+"use strict";
+
+
+$('*').css('border', '1px solid #000');
+
+// $('*').css('font-size', '1em');
+
+// $('test').hover(
+//     function(){
+//     $(this).hide()
+//     }, function () {
+//         $(this).show()
+//     }
+// );
+
+
+
+// IIFE closing //
+// })();
